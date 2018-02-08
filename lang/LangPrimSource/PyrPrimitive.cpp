e97627200700ca430fd533d1e9e891a8eac5229c
Maxime Sirbu <Maxime.Sirbu@ircam.fr>
2018-02-08 19:18:52 +0100

    Add CMake option SC_ABLETON fot link support

--- lang/LangPrimSource/PyrPrimitive.cpp
+++ lang/LangPrimSource/PyrPrimitive.cpp
@@ -4317,8 +4317,10 @@ void initCocoaFilePrimitives();
 void initSchedPrimitives();
 	initSchedPrimitives();
 
+#ifdef SC_ABLETON_LINK
 void initLinkPrimitives();
 	initLinkPrimitives();
+#endif
 
 #ifdef SC_HIDAPI
 void initHIDAPIPrimitives();
